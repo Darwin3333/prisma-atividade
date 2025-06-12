b9ca6d115923ca916cb164d04b514ffa77ff4a54
brunoNasc <emanuelbrunobn@rede.ulbra.br>
2025-06-11 22:56:03 -0300

    fix : resolvido problema de importação do prisma module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common';
 import { CoffeesModule } from './modules/coffees/coffees.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [CoffeesModule, PrismaModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
